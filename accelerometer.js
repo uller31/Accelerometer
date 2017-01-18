eedc61fe974399eb67aab3911fb149b4a3b9c0ff
uller31 <uller31@gmail.com>
2017-01-18 10:41:19 +0100

    afw

--- accelerometer.js
+++ accelerometer.js
@@ -1,35 +1,37 @@
 var watchID = null;
-//I onLoad tilføjes eventlistener
+
 function onLoad(){
-document.addEventListener("deviceready", onDeviceReady, false);
+	document.addEventListener("deviceready", onDeviceReady, false);
 }
-//kalder startfunktionen når enheden er klar
-function onDeviceReady() {
-startWatch();
+
+function onDeviceReady(){
+	startWatch();
+
 }
-//måler acceleration i bestemte intervaller
-function startWatch() {
-var options = { frequency: 1000 };//vis værdi hvert sekund
-watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options); //
+
+function startWatch(){
+	var options = {frequncy: 5};
+	watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
 }
-function stopWatch() {
-if (watchID) {
-navigator.accelerometer.clearWatch(watchID);
-watchID = null;
+
+function stopWatch(){
+	if (watchID){
+		navigator.accelerometer.clearWatch(watchID);
+		watchID = null;
+	}
 }
+
+function onSuccess(acceleration){
+	var accX = acceleration.x;
+	var accY = acceleration.y;
+	var accZ = acceleration.z;
+	var timestamp = acceleration.timestamp;
+	document.getElementById('accelerometer').innerHTML = 'Acceleration X: ' + accX + '<br>' + 
+	'Acceleration Y: ' + accY + '<br>' + 
+	'Acceleration Z: ' + accZ + '<br>' + 
+	'Timestamp: ' +timestamp + '<br>';
 }
-function onSuccess(acceleration) {
-//Gem sensorværdier
-var accX = acceleration.x ;
-var accY = acceleration.y;
-var accZ = acceleration.z;
-var timestamp = acceleration.timestamp;
-//Udskriv værdier i div med navnet accelerometer
-document.getElementById('accelerometer').innerHTML = 'Acceleration X: ' + accX + '<br/>' +
-'Acceleration Y: ' + accY + '<br />' +
-'Acceleration Z: ' + accZ + '<br />' +
-'Timestamp: ' + timestamp + '<br />';
-}
-function onError() {
-alert('onError!');
+
+function onError(){
+	alert('onError!');
 }
